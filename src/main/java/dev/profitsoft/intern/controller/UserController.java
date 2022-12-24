687c876938ee345717d89480e9f74b63268d780f
Roman Sierkin <serkin99@ukr.net>
2022-12-24 22:50:07 +0200

    add UserController

---
+++ src/main/java/dev/profitsoft/intern/controller/UserController.java
@@ -0,0 +1,27 @@
+package dev.profitsoft.intern.controller;
+
+import dev.profitsoft.intern.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ModelAndView showUsersPage() {
+        return new ModelAndView("users")
+                .addObject("users", userService.findAll());
+    }
+
+}
