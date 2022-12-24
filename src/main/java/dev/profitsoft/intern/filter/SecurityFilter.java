c741526fd7a9e8a76a99be1710e958f1948f65f9
Roman Sierkin <serkin99@ukr.net>
2022-12-24 22:53:18 +0200

    add SecurityFilter

---
+++ src/main/java/dev/profitsoft/intern/filter/SecurityFilter.java
@@ -0,0 +1,30 @@
+package dev.profitsoft.intern.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/users", "/hello", "/"})
+public class SecurityFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        if (request.getSession().getAttribute("authUser") != null) {
+            chain.doFilter(req, resp);
+        } else {
+            ((HttpServletResponse) resp).sendRedirect(request.getContextPath() + "/login");
+        }
+
+    }
+
+}
