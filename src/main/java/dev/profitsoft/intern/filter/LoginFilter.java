1a07fab5bbac8c9c0ebe29679d665e2ad3c9958a
Roman Sierkin <serkin99@ukr.net>
2022-12-24 22:53:31 +0200

    add LoginFilter

---
+++ src/main/java/dev/profitsoft/intern/filter/LoginFilter.java
@@ -0,0 +1,28 @@
+package dev.profitsoft.intern.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/login")
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        if (request.getSession().getAttribute("authUser") == null) {
+            chain.doFilter(req, resp);
+        } else {
+            ((HttpServletResponse) resp).sendError(403, "Authenticated user has no access to /login");
+        }
+    }
+
+}
