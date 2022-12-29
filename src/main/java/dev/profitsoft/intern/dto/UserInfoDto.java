b4bed0eefb95e9a11fc8afcb3181edad6faa1a6e
Roman Sierkin <serkin99@ukr.net>
2022-12-29 11:12:20 +0200

    add UserInfoDto

---
+++ src/main/java/dev/profitsoft/intern/dto/UserInfoDto.java
@@ -0,0 +1,16 @@
+package dev.profitsoft.intern.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserInfoDto {
+
+    private String username;
+
+    private String firstName;
+
+}
